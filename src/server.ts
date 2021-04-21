6507b337dcfa76c4194d560b73a20c68b77bc6bc
Gabriel Silva <devzgabriel@gmail.com>
2021-04-20 22:03:05 -0300

    config database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express, { request } from 'express'
+import express from 'express'
+
+import './database'
 
 const app = express()
-
-app.get('/', (request, response) => {
-  return response.send('Blaaaa')
-})
 
 app.listen(3333, () => console.log('Server running: 3333'))
